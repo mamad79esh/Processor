39d36f21b6cce6072b9c807eeb017abc5ce755cd
Sm_mE <mohammad79eshrati@gmail.com>
2020-07-18 16:53:37 +0430

    start commit

---
+++ src/simulator/Sample.java
@@ -0,0 +1,19 @@
+//Dedicated to Goli
+
+package simulator;
+
+import simulator.control.Simulator;
+import simulator.wrapper.wrappers.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        //sample circuit
+        Adder adder = new Adder("ADDER", "10X6",
+                Simulator.falseLogic, Simulator.trueLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.trueLogic,
+                Simulator.falseLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.trueLogic);
+
+        Simulator.debugger.addTrackItem(adder);
+        Simulator.debugger.setDelay(2000);
+        Simulator.circuit.startCircuit();
+    }
+}

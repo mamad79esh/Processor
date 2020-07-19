e7e76fdf7990e4a9bb3fdd4dbc96e487e8e859e6
Sm_mE <mohammad79eshrati@gmail.com>
2020-07-19 09:40:47 +0430

    Add multiplexers

--- src/simulator/Sample.java
+++ src/simulator/Sample.java
@@ -8,11 +8,18 @@ import simulator.wrapper.wrappers.*;
 public class Sample {
     public static void main(String[] args) {
         //sample circuit
-        Adder adder = new Adder("ADDER", "10X6",
-                Simulator.falseLogic, Simulator.trueLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.trueLogic,
-                Simulator.falseLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.trueLogic);
+//        Adder adder = new Adder("ADDER", "10X6",
+//                Simulator.falseLogic, Simulator.trueLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.trueLogic,
+//                Simulator.falseLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.trueLogic);
 
-        Simulator.debugger.addTrackItem(adder);
+//        Multiplexer2x1 m1 = new Multiplexer2x1("MUX_2x1","3X1",Simulator.falseLogic,Simulator.trueLogic,Simulator.falseLogic);
+//        Multiplexer4x1 m1 = new Multiplexer2x1("MUX_2x1","6X1",Simulator.falseLogic,Simulator.trueLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.trueLogic,Simulator.falseLogic);
+//
+
+        Multiplexer8x1 m1 = new Multiplexer8x1("MUX_8x1","11x1",Simulator.trueLogic,Simulator.falseLogic , Simulator.trueLogic,Simulator.trueLogic,Simulator.trueLogic,Simulator.trueLogic,Simulator.trueLogic,Simulator.trueLogic,Simulator.falseLogic,Simulator.trueLogic,Simulator.trueLogic);
+
+
+        Simulator.debugger.addTrackItem(m1);
         Simulator.debugger.setDelay(2000);
         Simulator.circuit.startCircuit();
     }

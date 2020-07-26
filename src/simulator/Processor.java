24f6ea845887052b537bac2adab28a5d682ff088
Sm_mE <mohammad79eshrati@gmail.com>
2020-07-26 09:54:45 +0430

    Update 7

--- src/simulator/Processor.java
+++ src/simulator/Processor.java
@@ -40,22 +40,22 @@ public class Processor {
         Multiplexer2x1 []writeRegisterData = new Multiplexer2x1[32];
         for (int i = 0 ; i < 32 ; i++)
             writeRegisterData[i] = new Multiplexer2x1("WriteRegisterData_"+i,"3X1");
-        
-        Multiplexer2x1 []writeRegisterDataSelect = new Multiplexer2x1[32];
-        for (int i = 0 ; i < 32 ; i++)
-            writeRegisterDataSelect[i] = new Multiplexer2x1("WriteRegisterDataSelect_"+i,"3X1");
 
-        
-        DFlipFlop[] shift = new DFlipFlop[2];
-        shift[0] = new DFlipFlop("SH0","2X2",clock.getOutput(0), Simulator.falseLogic);
-        shift[1] = new DFlipFlop("SH1","2X2",clock.getOutput(0), shift[0].getOutput(0));
-        
-        
-        for (int i=0 ; i<32 ; ++i)
-            writeRegisterDataSelect[i].addInput(shift[1].getOutput(0),
-                    writeRegisterData[i].getOutput(0),Simulator.falseLogic);
-        
-        
+//        Multiplexer2x1 []writeRegisterDataSelect = new Multiplexer2x1[32];
+//        for (int i = 0 ; i < 32 ; i++)
+//            writeRegisterDataSelect[i] = new Multiplexer2x1("WriteRegisterDataSelect_"+i,"3X1");
+//
+//
+//        DFlipFlop[] shift = new DFlipFlop[2];
+//        shift[0] = new DFlipFlop("SH0","2X2",clock.getOutput(0), Simulator.falseLogic);
+//        shift[1] = new DFlipFlop("SH1","2X2",clock.getOutput(0), shift[0].getOutput(0));
+//
+//
+//        for (int i=0 ; i<32 ; ++i)
+//            writeRegisterDataSelect[i].addInput(shift[1].getOutput(0),
+//                    writeRegisterData[i].getOutput(0),Simulator.falseLogic);
+
+
 
         //--------------------------------------------------------------------------------------------------------------
         // Pushing instructions to instruction memory
@@ -73,11 +73,55 @@ public class Processor {
             for(int j=0 ; j<8 ; j++)
                 instrucions[i][j] = true;
 
-        
-        
-        
-        // add $t0,$s1,$s2
-        
+
+//        // beq $t0,$s5,Exit(some address)
+//
+//        instrucions[0][0] = false;    instrucions[0][1] = false;
+//        instrucions[0][2] = false;    instrucions[0][3] = true;
+//        instrucions[0][4] = false;    instrucions[0][5] = false;
+//        instrucions[0][6] = false;     instrucions[0][7] = true;
+//
+//        instrucions[1][0] = false;    instrucions[1][1] = false;
+//        instrucions[1][2] = false;     instrucions[1][3] = true;
+//        instrucions[1][4] = false;    instrucions[1][5] = true;
+//        instrucions[1][6] = false;   instrucions[1][7] = true;
+//
+//        instrucions[2][0] = false;    instrucions[2][1] = false;
+//        instrucions[2][2] = false;     instrucions[2][3] = false;
+//        instrucions[2][4] = false;    instrucions[2][5] = false;
+//        instrucions[2][6] = false;   instrucions[2][7] = false;
+//
+//        instrucions[3][0] = false;    instrucions[3][1] = false;
+//        instrucions[3][2] = false;     instrucions[3][3] = false;
+//        instrucions[3][4] = false;    instrucions[3][5] = false;
+//        instrucions[3][6] = true;   instrucions[3][7] = false;
+//
+//        // or $s1,$zero,$s1
+//
+//        instrucions[0][0] = false;    instrucions[0][1] = false;
+//        instrucions[0][2] = false;    instrucions[0][3] = false;
+//        instrucions[0][4] = false;    instrucions[0][5] = false;
+//        instrucions[0][6] = true;     instrucions[0][7] = false;
+//
+//        instrucions[1][0] = false;    instrucions[1][1] = false;
+//        instrucions[1][2] = false;     instrucions[1][3] = true;
+//        instrucions[1][4] = false;    instrucions[1][5] = false;
+//        instrucions[1][6] = false;   instrucions[1][7] = true;
+//
+//        instrucions[2][0] = true;    instrucions[2][1] = false;
+//        instrucions[2][2] = false;     instrucions[2][3] = false;
+//        instrucions[2][4] = true;    instrucions[2][5] = false;
+//        instrucions[2][6] = false;   instrucions[2][7] = false;
+//
+//        instrucions[3][0] = false;    instrucions[3][1] = false;
+//        instrucions[3][2] = true;     instrucions[3][3] = false;
+//        instrucions[3][4] = false;    instrucions[3][5] = true;
+//        instrucions[3][6] = true;   instrucions[3][7] = false;
+
+
+
+//         add $t0,$s1,$s2
+
         instrucions[0][0] = false;    instrucions[0][1] = false;
         instrucions[0][2] = false;    instrucions[0][3] = false;
         instrucions[0][4] = false;    instrucions[0][5] = false;
@@ -88,72 +132,72 @@ public class Processor {
         instrucions[1][4] = false;    instrucions[1][5] = false;
         instrucions[1][6] = true;   instrucions[1][7] = false;
 
-      instrucions[2][0] = false;    instrucions[2][1] = true;
-      instrucions[2][2] = false;     instrucions[2][3] = false;
-      instrucions[2][4] = false;    instrucions[2][5] = false;
-      instrucions[2][6] = false;   instrucions[2][7] = false;
+        instrucions[2][0] = false;    instrucions[2][1] = true;
+        instrucions[2][2] = false;     instrucions[2][3] = false;
+        instrucions[2][4] = false;    instrucions[2][5] = false;
+        instrucions[2][6] = false;   instrucions[2][7] = false;
 
-      instrucions[3][0] = false;    instrucions[3][1] = false;
-      instrucions[3][2] = true;     instrucions[3][3] = false;
-      instrucions[3][4] = false;    instrucions[3][5] = false;
-      instrucions[3][6] = false;   instrucions[3][7] = false;
-        
-        
-      // sw  $t0,0($t1)
-      
-    instrucions[4][0] = true;     instrucions[4][1] = false;
-    instrucions[4][2] = true;    instrucions[4][3] = false;
-    instrucions[4][4] = true;     instrucions[4][5] = true;
-    instrucions[4][6] = false;    instrucions[4][7] = true;
+        instrucions[3][0] = false;    instrucions[3][1] = false;
+        instrucions[3][2] = true;     instrucions[3][3] = false;
+        instrucions[3][4] = false;    instrucions[3][5] = false;
+        instrucions[3][6] = false;   instrucions[3][7] = false;
 
-    instrucions[5][0] = false;    instrucions[5][1] = false;
-    instrucions[5][2] = true;     instrucions[5][3] = false;
-    instrucions[5][4] = true;     instrucions[5][5] = false;
-    instrucions[5][6] = false;    instrucions[5][7] = false;
 
-    instrucions[6][0] = false;    instrucions[6][1] = false;
-    instrucions[6][2] = false;    instrucions[6][3] = false;
-    instrucions[6][4] = false;    instrucions[6][5] = false;
-    instrucions[6][6] = false;    instrucions[6][7] = false;
+        // sw  $t0,0($t1)
 
-    instrucions[7][0] = false;    instrucions[7][1] = false;
-    instrucions[7][2] = false;    instrucions[7][3] = false;
-    instrucions[7][4] = false;    instrucions[7][5] = false;
-    instrucions[7][6] = false;    instrucions[7][7] = false;  
-        
-        
-        
-    // lw  $t0,0($t1)
-    
-    instrucions[8][0] = true;     instrucions[8][1] = false;
-    instrucions[8][2] = false;    instrucions[8][3] = false;
-    instrucions[8][4] = true;     instrucions[8][5] = true;
-    instrucions[8][6] = false;    instrucions[8][7] = true;
+        instrucions[4][0] = true;     instrucions[4][1] = false;
+        instrucions[4][2] = true;    instrucions[4][3] = false;
+        instrucions[4][4] = true;     instrucions[4][5] = true;
+        instrucions[4][6] = false;    instrucions[4][7] = true;
 
-    instrucions[9][0] = false;    instrucions[9][1] = false;
-    instrucions[9][2] = true;     instrucions[9][3] = false;
-    instrucions[9][4] = true;     instrucions[9][5] = false;
-    instrucions[9][6] = false;    instrucions[9][7] = false;
+        instrucions[5][0] = false;    instrucions[5][1] = false;
+        instrucions[5][2] = true;     instrucions[5][3] = false;
+        instrucions[5][4] = true;     instrucions[5][5] = false;
+        instrucions[5][6] = false;    instrucions[5][7] = false;
 
-    instrucions[10][0] = false;    instrucions[10][1] = false;
-    instrucions[10][2] = false;    instrucions[10][3] = false;
-    instrucions[10][4] = false;    instrucions[10][5] = false;
-    instrucions[10][6] = false;    instrucions[10][7] = false;
+        instrucions[6][0] = false;    instrucions[6][1] = false;
+        instrucions[6][2] = false;    instrucions[6][3] = false;
+        instrucions[6][4] = false;    instrucions[6][5] = false;
+        instrucions[6][6] = false;    instrucions[6][7] = false;
 
-    instrucions[11][0] = false;    instrucions[11][1] = false;
-    instrucions[11][2] = false;    instrucions[11][3] = false;
-    instrucions[11][4] = false;    instrucions[11][5] = false;
-    instrucions[11][6] = false;    instrucions[11][7] = false;  
-    
-    
-    
-    
-    
-    
-        
-        
-        
-        
+        instrucions[7][0] = false;    instrucions[7][1] = false;
+        instrucions[7][2] = false;    instrucions[7][3] = false;
+        instrucions[7][4] = false;    instrucions[7][5] = false;
+        instrucions[7][6] = false;    instrucions[7][7] = false;
+
+
+
+        // lw  $t0,0($t1)
+
+        instrucions[8][0] = true;     instrucions[8][1] = false;
+        instrucions[8][2] = false;    instrucions[8][3] = false;
+        instrucions[8][4] = true;     instrucions[8][5] = true;
+        instrucions[8][6] = false;    instrucions[8][7] = true;
+
+        instrucions[9][0] = false;    instrucions[9][1] = false;
+        instrucions[9][2] = true;     instrucions[9][3] = false;
+        instrucions[9][4] = true;     instrucions[9][5] = false;
+        instrucions[9][6] = false;    instrucions[9][7] = false;
+
+        instrucions[10][0] = false;    instrucions[10][1] = false;
+        instrucions[10][2] = false;    instrucions[10][3] = false;
+        instrucions[10][4] = false;    instrucions[10][5] = false;
+        instrucions[10][6] = false;    instrucions[10][7] = false;
+
+        instrucions[11][0] = false;    instrucions[11][1] = false;
+        instrucions[11][2] = false;    instrucions[11][3] = false;
+        instrucions[11][4] = false;    instrucions[11][5] = false;
+        instrucions[11][6] = false;    instrucions[11][7] = false;
+//
+
+
+
+
+
+
+
+
+
 
         ////////////////////////////////////////////////////////
 
@@ -271,8 +315,11 @@ public class Processor {
         for (int i = 17 ; i < 33 ; i++){// add address
             dataMemory.addInput(alu.getOutput(i));
         }
-        for (int i = 0 ; i < 32 ; i++){// add write data
+        for (int i = 0 ; i < 32 ; i++){// add data
+
             dataMemory.addInput(registerFile.getOutput(32+i));
+           // dataMemory.addInput(Simulator.falseLogic);
+
         }
 
         //--------------------------------------------------------------------------------------------------------------
@@ -290,7 +337,14 @@ public class Processor {
 
 
         Simulator.debugger.addTrackItem(clock,dataMemory,registerFile,alu,instructionMemory,
-                pcUpdate,controlUnit,aluControl);
+                pcUpdate,controlUnit,aluControl,
+                writeRegisterData[0],writeRegisterData[1],writeRegisterData[2],writeRegisterData[3],writeRegisterData[4],
+                writeRegisterData[5],writeRegisterData[6],writeRegisterData[7],writeRegisterData[8],writeRegisterData[9],
+                writeRegisterData[10],writeRegisterData[11],writeRegisterData[12],writeRegisterData[13],writeRegisterData[14],
+                writeRegisterData[15],writeRegisterData[16],writeRegisterData[17],writeRegisterData[18],writeRegisterData[19],
+                writeRegisterData[20],writeRegisterData[21],writeRegisterData[22],writeRegisterData[23],writeRegisterData[24],
+                writeRegisterData[25],writeRegisterData[26],writeRegisterData[27],writeRegisterData[28],writeRegisterData[29],
+                writeRegisterData[30],writeRegisterData[31]);
 
         Simulator.debugger.setDelay(500);
         Simulator.circuit.startCircuit();
--- src/simulator/test.java
+++ src/simulator/test.java
@@ -3,20 +3,26 @@ package simulator;
 import simulator.control.Simulator;
 import simulator.gates.combinational.ByteMemory;
 import simulator.gates.sequential.Clock;
-import simulator.wrapper.wrappers.ALU;
 import simulator.wrapper.wrappers.DFlipFlop;
 
 public class test {
     public static void main(String[] args) {
         // Creating needed wrapper :
-        ALU alu = new ALU("ALU","68X33",Simulator.falseLogic,Simulator.trueLogic,Simulator.trueLogic,Simulator.falseLogic);
+        Clock clock = new Clock("Clock",2000);
+        DFlipFlop d = new DFlipFlop("CLK","2x2",clock.getOutput(0));
+        d.addInput(d.getOutput(1));
+
+        ByteMemory mem = new ByteMemory("Mem",clock.getOutput(0));
+        for (int i = 0 ; i < 16 ; i++)
+            mem.addInput(Simulator.falseLogic);
 
         for (int i = 0 ; i < 32 ; i++)
-            alu.addInput(Simulator.trueLogic);
+            mem.addInput(Simulator.falseLogic);
 
-        for (int i = 0 ; i < 32 ; i++)
-            alu.addInput(Simulator.falseLogic);
-        Simulator.debugger.addTrackItem(alu);
+
+
+
+        Simulator.debugger.addTrackItem(mem,clock);
         Simulator.debugger.setDelay(500);
         Simulator.circuit.startCircuit();
     }
